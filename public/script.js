439a4b634296f92d7aed28133f65286c7b501ae9
DidierLe3 <68671830+DidierLe3@users.noreply.github.com>
2020-11-08 23:47:52 -0500

    Update script.js
    Co-Authored-By: jsong09 <70549697+jsong09@users.noreply.github.com>
    Co-Authored-By: lit95 <25183657+lit95@users.noreply.github.com>

--- public/script.js
+++ public/script.js
@@ -1,52 +1,44 @@
-const PGfoodSafetyData = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-
-const placeName = []; //an empty arrray to store the restaurants' name
-const category = []; //an empty arrray to store the restaurants' category
-
+const pgFoodSafetyData = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  
+const placeName = [];
 // fetch the data
-fetch(PGfoodSafetyData) // fetch returns a promise
-    .then(blob => blob.json())
+fetch(pgFoodSafetyData) // fetch returns a promise
+    .then(contentOfPG => contentOfPG.json())
     .then(data => placeName.push(...data)) // because I want restaurant to stay constant
 // the ... spread into a function
-    // the data that comes back from the fetch can be image, music, HTML, ect
+// the data that comes back from the fetch can be image, music, HTML, ect
 // the fecth ignore the type of data
 // get data into restaurant
 
-function findNameOrCategorty(textMatch, placeName){
+function findNameOrCategory(textMatch, placeName){
     return placeName.filter(place => {
         /**check if the text typed has a match */
         const regex = new RegExp(textMatch, 'gi'); // gi = global insensitive (it ignore UP or Low cases)
-        return place.name.match(regex) ||
-        place.category.match(regex);  
+        return place.name.match(regex) || place.category.match(regex)  
     });
-}/**end find() */
+}
 
-function displayNameOrCategorty(){
-    const matchArray = findNameOrCategorty(this.value, placeName);
-    const html = matchArray.map(place => {
-        const regularExpression = new RegExp(this.value, 'gi');/**highlight the matching word */
-        const nameOfThePlace = place.name.replace(regex, 
-            `<span class="h1">${this.value}</span>`);
 
-        const categoryFood = place.category.replace(regex, 
-             `<span class="h1">${this.value}</span>`);
+function displayNameOrCategory() {
+  const matchArray = findNameOrCategory(this.value, placeName);
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi'); /**highlight the matching word */
 
-        return `
-        <li>        
-            <span class="name">${nameOfThePlace}</span>
-        </li>
-    
-        <li>        
-            <span class="category">${categoryFood}</span>
-        </li>
-        `;
-    }).join('');
-    suggestions.innerHTML = html;
-} /**end displayMatches() */
+    const nameOfThePlace = place.name.replace(regex, `<span class="h2">${this.value}</span>`);
+    const categoryFood = place.category.replace(regex, `<span class="h2">${this.value}</span>`);
+    return `<div class="output">
+      <li>
+        <span class="name">${nameOfThePlace}, ${categoryFood}</span>
+        <span class="address">${place.address_line_1}</span>
+      </li>
+      </div>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+}
 
-const textLookUp = document.querySelector('.textInput');
-const hint = document.querySelector('.suggestions');
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 
-textLookUp = addEventListener('change', displayNameOrCategorty);
-textLookUp = addEventListener('keyup', displayNameOrCategorty);
-
+searchInput.addEventListener('change', displayNameOrCategory);
+searchInput.addEventListener('keyup', displayNameOrCategory);
